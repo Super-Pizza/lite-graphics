acaa8a07f40145e494f7674fa44e7d31288a90a5
Fabien Cournoyer <fabien@cournoyer.club>
2025-03-17 12:06:32 -0400

    Add even more impls!

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Sub};
+use std::ops::{Add, Sub, SubAssign};
 
 pub mod draw;
 #[cfg(feature = "window")]
@@ -51,7 +51,7 @@ impl Rect {
             x: self.x.max(other.x),
             y: self.y.max(other.y),
         };
-        let size = end - offs;
+        let size = end.abs_diff(offs);
         (offs, size).into()
     }
 }
@@ -65,6 +65,12 @@ pub struct Offset {
 impl Offset {
     pub fn new(x: i32, y: i32) -> Self {
         Self { x, y }
+    }
+    pub fn abs_diff(&self, rhs: Self) -> Size {
+        Size {
+            w: self.x.abs_diff(rhs.x),
+            h: self.y.abs_diff(rhs.y),
+        }
     }
 }
 
@@ -162,64 +168,118 @@ impl From<Offset> for (i32, i32) {
     }
 }
 
-impl Sub<Self> for Offset {
-    type Output = Size;
-    #[inline]
-    fn sub(self, rhs: Self) -> Self::Output {
+macro_rules! ref_impls {
+    (impl $impl:ident<$other:ty> for $self:ty { fn $fn:ident($lhs:ident, $rhs:ident) -> $out:ty { $($code:tt)* } }) => {
+        impl $impl<$other> for $self {
+            type Output = $out;
+            #[inline]
+            fn $fn(self, rhs: $other) -> Self::Output {
+                let $lhs = self;
+                let $rhs = rhs;
+                $($code)*
+            }
+        }
+        impl $impl<$other> for &$self {
+            type Output = $out;
+            #[inline]
+            fn $fn(self, rhs: $other) -> Self::Output {
+                let $lhs = *self;
+                let $rhs = rhs;
+                $($code)*
+            }
+        }
+        impl $impl<&$other> for $self {
+            type Output = $out;
+            #[inline]
+            fn $fn(self, rhs: &$other) -> Self::Output {
+                let $lhs = self;
+                let $rhs = *rhs;
+                $($code)*
+            }
+        }
+        impl $impl<&$other> for &$self {
+            type Output = $out;
+            #[inline]
+            fn $fn(self, rhs: &$other) -> Self::Output {
+                let $lhs = *self;
+                let $rhs = *rhs;
+                $($code)*
+            }
+        }
+    };
+    (impl $impl:ident<$other:ty> for $self:ty { fn $fn:ident(&mut $lhs:ident, $rhs:ident) { $($code:tt)* } }) => {
+        impl $impl<$other> for $self {
+            #[inline]
+            fn $fn(&mut self, rhs: $other) {
+                let $lhs = self;
+                let $rhs = rhs;
+                $($code)*
+            }
+        }
+        impl $impl<&$other> for $self {
+            #[inline]
+            fn $fn(&mut self, rhs: &$other) {
+                let $lhs = self;
+                let $rhs = *rhs;
+                $($code)*
+            }
+        }
+    };
+}
+
+ref_impls! {impl Sub<Self> for Offset {
+    fn sub(this, rhs) -> Offset {
+        Offset {
+            x: this.x - rhs.x,
+            y: this.y - rhs.y
+        }
+    }
+}}
+
+ref_impls! {impl SubAssign<Self> for Offset {
+    fn sub_assign(&mut this, rhs) {
+        *this = *this - rhs
+    }
+}}
+
+ref_impls! {impl Add<Size> for Offset {
+    fn add(this, rhs) -> Offset {
+        Offset {
+            x: this.x + rhs.w as i32,
+            y: this.y + rhs.h as i32,
+        }
+    }
+}}
+
+ref_impls! {impl Add<Offset> for Size {
+    fn add(this, rhs) -> Offset {
+        Offset {
+            x: this.w as i32 + rhs.x,
+            y: this.h as i32 + rhs.y,
+        }
+    }
+}}
+
+ref_impls! {impl Add<Self> for Offset {
+    fn add(this, rhs) -> Offset {
+        Offset {
+            x: this.x + rhs.x,
+            y: this.y + rhs.y,
+        }
+    }
+}}
+
+ref_impls! {impl Add<Self> for Size {
+    fn add(this, rhs) -> Size {
         Size {
-            w: (self.x - rhs.x).max(0) as u32,
-            h: (self.y - rhs.y).max(0) as u32,
+            w: this.w + rhs.w,
+            h: this.h + rhs.h,
         }
     }
-}
+}}
 
-impl Add<Size> for Offset {
-    type Output = Offset;
-    #[inline]
-    fn add(self, rhs: Size) -> Self::Output {
-        Offset {
-            x: self.x + rhs.w as i32,
-            y: self.y + rhs.h as i32,
-        }
+ref_impls! {impl Add<Offset> for Rect {
+    fn add(this, rhs) -> Rect {
+        Rect::from((this.offset() + rhs, this.size()))
     }
-}
-
-impl Add<Offset> for Size {
-    type Output = Offset;
-    #[inline]
-    fn add(self, rhs: Offset) -> Self::Output {
-        Offset {
-            x: self.w as i32 + rhs.x,
-            y: self.h as i32 + rhs.y,
-        }
-    }
-}
-
-impl Add<Self> for Offset {
-    type Output = Offset;
-    #[inline]
-    fn add(self, rhs: Self) -> Self::Output {
-        Offset {
-            x: self.x + rhs.x,
-            y: self.y + rhs.y,
-        }
-    }
-}
-
-impl Add<Self> for Size {
-    type Output = Size;
-    #[inline]
-    fn add(self, rhs: Self) -> Self::Output {
-        Size {
-            w: self.w + rhs.w,
-            h: self.h + rhs.h,
-        }
-    }
-}
-
-impl Add<Offset> for Rect {
-    type Output = Rect;
-    fn add(self, rhs: Offset) -> Self::Output {
-        Rect::from((self.offset() + rhs, self.size()))
-    }
-}
+}}
