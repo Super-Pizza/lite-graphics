5c84a366099a5a7140635a847b404aa59c6f4549
Fabien Cournoyer <fabien@cournoyer.club>
2025-04-01 05:18:35 -0700

    Adjust cfg directives

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -1,10 +1,40 @@
-#[cfg(feature = "wayland")]
+#[cfg(all(
+    unix,
+    feature = "wayland",
+    not(any(
+        target_os = "redox",
+        target_family = "wasm",
+        target_os = "android",
+        target_os = "ios",
+        target_os = "macos"
+    ))
+))]
 mod wayland;
-#[cfg(feature = "x11rb")]
+#[cfg(all(
+    unix,
+    feature = "x11rb",
+    not(any(
+        target_os = "redox",
+        target_family = "wasm",
+        target_os = "android",
+        target_os = "ios",
+        target_os = "macos"
+    ))
+))]
 mod x11;
 
 impl crate::draw::Buffer {
     pub fn draw(&self) {
+        #[cfg(all(
+            unix,
+            not(any(
+                target_os = "redox",
+                target_family = "wasm",
+                target_os = "android",
+                target_os = "ios",
+                target_os = "macos"
+            ))
+        ))]
         if std::env::var("WAYLAND_DISPLAY")
             .unwrap_or_default()
             .is_empty()
