e89340b5302daa15de8c1144694b2b24257c4b84
Fabien Cournoyer <fabien@cournoyer.club>
2025-07-01 10:02:42 -0400

    make Rgba fields public

--- src/color.rs
+++ src/color.rs
@@ -7,10 +7,10 @@ pub trait Color: Clone {
 
 #[derive(Clone, Copy)]
 pub struct Rgba {
-    r: u8,
-    g: u8,
-    b: u8,
-    a: u8,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
 }
 
 impl From<[u8; 4]> for Rgba {
